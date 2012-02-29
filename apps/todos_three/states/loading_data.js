c96052f525873d26bdfadde54117235018711a72
Tim Evans <tim-evans@sproutcore.com>
2012-02-29 16:11:32 -0500

    sort Todos by the most recent to the least recent

--- apps/todos_three/states/loading_data.js
+++ apps/todos_three/states/loading_data.js
@@ -1,7 +1,7 @@
 TodosThree.LOADING_DATA = SC.State.design({
   enterState: function () {
     TodosThree.todosController.set('content',
-      TodosThree.store.find(SC.Query.local(TodosThree.Todo)));
+      TodosThree.store.find(SC.Query.local(TodosThree.Todo, { orderBy: 'timestamp DESC' })));
 
     this.gotoState('SHOWING_APP');
   },
