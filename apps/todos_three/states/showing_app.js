fdebab0d9ca71b0571b9a42a14e0b4f49205ca55
Tim Evans <tim-evans@sproutcore.com>
2012-02-29 16:05:28 -0500

    add 'addTodo' action to the statechart

--- apps/todos_three/states/showing_app.js
+++ apps/todos_three/states/showing_app.js
@@ -3,5 +3,12 @@ TodosThree.SHOWING_APP = SC.State.design({
     TodosThree.getPath('mainPage.mainPane').append();
   },
 
-  exitState: function () {}
+  exitState: function () {},
+
+  addTodo: function (view) {
+    TodosThree.store.createRecord(TodosThree.Todo, {
+      title: view.get('value'),
+      timestamp: SC.DateTime.create()
+    });
+  }
 });
