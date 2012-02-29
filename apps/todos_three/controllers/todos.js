69475986a756f9347d77769cea639bd32955063d
Tim Evans <tim-evans@sproutcore.com>
2012-02-29 16:24:48 -0500

    added 'areAllCompleted' computed property to todosController

--- apps/todos_three/controllers/todos.js
+++ apps/todos_three/controllers/todos.js
@@ -1,1 +1,13 @@
-TodosThree.todosController = SC.ArrayController.create();
+sc_require('controllers/completed_todos');
+
+TodosThree.todosController = SC.ArrayController.create({
+
+  completedTodosBinding: SC.Binding.oneWay('TodosThree.completedTodosController.length'),
+
+  areAllCompleted: function (k, v) {
+    if (v !== undefined) {
+      this.setEach('isCompleted', v);
+    }
+    return this.get('length') === this.get('completedTodos');
+  }.property('length', 'completedTodos').cacheable()
+});
