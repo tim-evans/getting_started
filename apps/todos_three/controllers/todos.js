d2c776998d4787f86cfd90e0e0d904f501b825c9
Tim Evans <tim-evans@sproutcore.com>
2012-02-29 13:02:02 -0500

    add todosController to hold all todos

---
+++ apps/todos_three/controllers/todos.js
@@ -0,0 +1,1 @@
+TodosThree.todosController = SC.ArrayController.create();
--- apps/todos_three/states/loading_data.js
+++ apps/todos_three/states/loading_data.js
@@ -1,5 +1,8 @@
 TodosThree.LOADING_DATA = SC.State.design({
   enterState: function () {
+    TodosThree.todosController.set('content',
+      TodosThree.store.find(SC.Query.local(TodosThree.Todo)));
+
     this.gotoState('SHOWING_APP');
   },
 
