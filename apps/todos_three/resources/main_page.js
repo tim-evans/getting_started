767966dcfd84c1363562511176f960e1ce3ac6be
Tim Evans <tim-evans@sproutcore.com>
2012-02-29 13:06:40 -0500

    add a list view to show the current Todos

--- apps/todos_three/resources/main_page.js
+++ apps/todos_three/resources/main_page.js
@@ -11,13 +11,11 @@ TodosThree.mainPage = SC.Page.design({
   // Add childViews to this pane for views to display immediately on page 
   // load.
   mainPane: SC.MainPane.design({
-    childViews: 'labelView'.w(),
+    childViews: 'todosList'.w(),
     
-    labelView: SC.LabelView.design({
-      layout: { centerX: 0, centerY: 0, width: 200, height: 18 },
-      textAlign: SC.ALIGN_CENTER,
-      tagName: "h1", 
-      value: "Welcome to SproutCore!"
+    todosList: SC.ListView.design({
+      layout: { centerX: 0, width: 500, top: 0, bottom: 0 },
+      contentBinding: SC.Binding.oneWay('TodosThree.todosController')
     })
   })
 
