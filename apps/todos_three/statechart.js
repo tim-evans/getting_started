8562b0b3263cf4845dfcf1ab84da5771420ac2a4
Tim Evans <tim-evans@sproutcore.com>
2012-03-02 10:50:36 -0500

    remove loading_data with starting

--- apps/todos_three/statechart.js
+++ apps/todos_three/statechart.js
@@ -5,8 +5,6 @@ TodosThree.statechart = SC.Statechart.create({
 
     STARTING: SC.State.plugin("TodosThree.STARTING"),
 
-    LOADING_DATA: SC.State.plugin("TodosThree.LOADING_DATA"),
-
     SHOWING_APP: SC.State.plugin("TodosThree.SHOWING_APP")
   })
 });
--- apps/todos_three/states/starting.js
+++ apps/todos_three/states/starting.js
@@ -1,8 +1,16 @@
 TodosThree.STARTING = SC.State.extend({
 
   enterState: function () {
-    this.gotoState('LOADING_DATA');
+    TodosThree.todosController.set('content',
+      TodosThree.store.find(SC.Query.local(TodosThree.Todo, { orderBy: 'timestamp DESC' })));
+
+    TodosThree.completedTodosController.set('content',
+      TodosThree.store.find(SC.Query.local(TodosThree.Todo, 'isCompleted = YES')));
   },
+
+  didLoad: function () {
+    this.gotoState('SHOWING_APP');
+  }.stateObserves('TodosThree.todosController*content.status'),
 
   exitState: function () {
     // Nothing to worry about here
